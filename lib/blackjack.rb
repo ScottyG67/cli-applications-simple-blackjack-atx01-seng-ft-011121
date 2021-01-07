2c7ddae844a179fe111925d5daae29bd6fefd7fe
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-07 17:29:26 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -32,8 +32,8 @@ end
 def initial_round
   # code #initial_round here
   binding.pry
-  card_total = deal_card
-  card_total = card_total + deal_card
+  card_total = deal_card + deal_card
+  #card_total = card_total + deal_card
   binding.pry
   display_card_total(card_total)
 end
