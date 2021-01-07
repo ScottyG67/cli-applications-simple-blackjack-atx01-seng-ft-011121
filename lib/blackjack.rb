c7ef03f742159bffe4dc6941ccab114b969f8869
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-07 17:15:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -11,9 +11,9 @@ end
 
 def display_card_total(card_total)
   # code #display_card_total here
-  binding.pry
+  #binding.pry
   card_total=card_total+deal_card
-  binding.pry
+  #binding.pry
   puts card_total
 end
 
