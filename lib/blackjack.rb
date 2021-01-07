720e812be63b89133bb860219d82c111356aef07
Scott Gloyna <scott.gloyna01@gmail.com>
2021-01-07 17:39:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -7,6 +7,7 @@ end
 def deal_card
   # code #deal_card here
   rand(1..11)
+  binding.pry
 end
 
 def display_card_total(card_total)
@@ -38,6 +39,7 @@ end
 
 def hit?
   # code hit? here
+  
 end
 
 def invalid_command
